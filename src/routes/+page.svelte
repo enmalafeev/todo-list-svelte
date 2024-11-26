bd37d51c386793e3a5eadff0d0a724ff6323da81
Evgeny Malafeev <malafeev.en@yandex.ru>
2024-11-26 14:36:40 +0300

    add main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import TodoForm from '../components/TodoForm.svelte';
+	import TodoList from '../components/TodoList.svelte';
+
+	interface Todo {
+		id: number;
+		text: string;
+	}
+
+	let todos = $state<Todo[]>([]);
+
+	const addTodo = (text: string) => {
+		const newTodo = {
+			id: Date.now(),
+			text: text
+		};
+		todos.push(newTodo);
+	};
+</script>
+
+<h1>Todo App</h1>
+<TodoForm onChange={addTodo} />
+<TodoList {todos} />
