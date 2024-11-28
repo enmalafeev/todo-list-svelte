5933a48e884f051aa2682e91b64d3c5855f0a873
Evgeny Malafeev <malafeev.en@yandex.ru>
2024-11-28 14:17:50 +0300

    add TodoApp component

---
+++ src/components/TodoApp.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import TodoForm from '../components/TodoForm.svelte';
+	import TodoList from '../components/TodoList.svelte';
+	import { validateTodo } from '../utils/validateTodo';
+
+	interface Todo {
+		id: number;
+		text: string;
+	}
+
+	let todos = $state<Todo[]>([]);
+	let errors: string[] = $state([]);
+
+	const addTodo = (text: string) => {
+		errors = validateTodo(text);
+		if (errors.length === 0) {
+			const newTodo = {
+				id: Date.now(),
+				text: text
+			};
+			todos.push(newTodo);
+		}
+	};
+</script>
+
+<h1>Todo App</h1>
+<TodoForm onChange={addTodo} {errors} />
+<TodoList {todos} />
--- src/tests/TodoForm.test.ts
+++ src/tests/TodoForm.test.ts
@@ -5,7 +5,7 @@ import TodoForm from '../components/TodoForm.svelte';
 describe('TodoForm test', () => {
 	const fn = vi.fn();
 	it('should update the value when typing', async () => {
-		const { getByRole } = render(TodoForm, { props: { onChange: fn() } });
+		const { getByRole } = render(TodoForm, { props: { onChange: fn(), errors: [] } });
 
 		const input = getByRole('textbox') as HTMLInputElement;
 		expect(input.value).toBe('');
@@ -16,7 +16,7 @@ describe('TodoForm test', () => {
 
 	it('should call onChange handler with updated value', async () => {
 		const mockOnChange = vi.fn(() => 'test');
-		const { getByRole } = render(TodoForm, { props: { onChange: mockOnChange } });
+		const { getByRole } = render(TodoForm, { props: { onChange: mockOnChange, errors: [] } });
 
 		const input = getByRole('textbox');
 		const button = getByRole('button');
